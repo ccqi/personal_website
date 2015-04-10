81504056cc0fc8905ba9a535c90efd4028bfd632
Charles Qi <ccqi@uwaterloo.ca>
2015-04-09 23:00:30 -0400

    initial project setup and commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/static'));
+app.get('/', function (req, res) {
+  res.sendfile('static/index.html',{root:__dirname});
+});
+
+var server = app.listen(8080, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://localhost:8080...');
+
+});
