3502a8a27a04c5d7825434728535401e2b8345fc
Ubuntu <ubuntu@ip-172-31-18-203.us-west-2.compute.internal>
2015-05-18 19:37:24 +0000

    redirect port from 80 to 3000

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post('/sendEmail',function(req,res){
 });
 
 
-var server = app.listen(8080, function () {
+var server = app.listen(3000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
